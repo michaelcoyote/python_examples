ac69c36893c7430ef29d2add05942445f8bbc894
michael guldan <michaelguldan@gmail.com>
2017-08-10 23:34:18 -0700

    Add simple check on data class inputs

--- dataclass.py
+++ dataclass.py
@@ -5,7 +5,19 @@ class TestObj(object):
     """A test object for data."""
 
     def __init__(self, dat1, dat2, dat3):
-        """Return a new TestObj object"""
-        self.dat1 = dat1
+        """Return a new TestObj object.
+
+        Make sure that dat1 is shorter than 10 characters.
+        """
+        if len(dat1) < 10:
+            self.dat1 = dat1
+        else:
+            raise Exception("dat1 is longer than 10 char: {}" % dat1)
+
         self.dat2 = dat2
         self.dat3 = dat3
+
+    def __str__(self):
+        """Stringify data from object."""
+        return('dat1: {} dat2: {} dat3: {}'
+               .format(self.dat1, self.dat2, self.dat3))
