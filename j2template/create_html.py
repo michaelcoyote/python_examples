754cf923cb3efa4f15fa2616cc68c1861f3f6068
michael guldan <michaelguldan@gmail.com>
2018-11-29 00:56:52 -0800

    Jinja examples with templates and simple flask app

---
+++ j2template/create_html.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from jinja2 import Environment, FileSystemLoader
+env = Environment(loader=FileSystemLoader('templates'))
+template = env.get_template('template.html')
+output_from_parsed_template = template.render(my_string="Wheeeee!",
+                                              my_list=[0, 1, 2, 3, 4, 5])
+print output_from_parsed_template
+
+# to save the results
+# with open("my_rendered_file.html", "wb") as fh:
+#    fh.write(output_from_parsed_template)
---
+++ j2template/gen_config.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+"""Jinja conf file generator POC."""
+
+from jinja2 import Environment, FileSystemLoader
+# import yaml
+
+# def conf_file(conf_file_path):
+#     with open(conf_file_path) as conf_fh
+#     return yaml.safe_load(conf_fh)
+
+
+def config_gen(conf_file, conf_data):
+    """Read the template and render config."""
+    # set up the template dir and set trim_block to remove extra whitespace
+    t_path = Environment(
+            loader=FileSystemLoader('./templates/'),
+            trim_blocks=True)
+    template = t_path.get_template(conf_file + '.j2')
+    return template.render(conf_data=conf_data)
+
+
+def main():
+    conf_data = {
+            'a_list': [1, 2, 3, 4, 5],
+            'b_dict': {
+                'one': '#1',
+                'two': '#2',
+                'three': '#3',
+                },
+            'c_nested': {
+                'header_1': {
+                    'sub1a': '1a',
+                    'sub1b': '1b',
+                    },
+                'header_2': {
+                    'sub2a': '2a',
+                    'sub2b': '2b',
+                    'sub2c': '2c',
+                    },
+                'header_3': {
+                    'sub3a': '3a'
+                    }
+                },
+            'd_empty': None,
+            }
+
+    # print conf_data
+    rendered_t = config_gen(conf_file='test.conf', conf_data=conf_data)
+    print rendered_t
+
+
+if __name__ == "__main__":
+    main()
