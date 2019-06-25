94d6a2f09a57b3cd17512cb22ab093cd2c9b5895
michael guldan <michaelguldan@gmail.com>
2019-06-25 00:26:00 -0700

    Sorting problems and tests for hackerrank
    Added introductory test and analsys for insert sort.  Will need to
    revisit analsys as it times out and I only got partial credit (3/50).

--- hackerrank/algorithm_sorting.py
+++ hackerrank/algorithm_sorting.py
@@ -2,7 +2,57 @@
 
 class Sorting(object):
 
+    def intro_tutorial(self, V, arr):
+        '''Find location of number in array.
+
+        Given a sorted array (arr) and a number(V), print index 
+        location of V.
+
+        Input:
+        arr: a sorted array of integers
+        V: an integer to search for
+
+        Output: Index of V
+        '''
+        count = 0
+        for i in arr:
+            if i == V:
+                return count
+            count += 1
+        # return arr.index(V)
+
     def median(self, arr):
         '''Return the median of an array w/ odd elements.'''
         arr.sort()
         return arr[int((len(arr) - 1) / 2)]
+
+    def insertion_sort_analyse(self, arr):
+        '''Analyse the amount of work required to sort array.
+
+        Calculate number of shifts insertion sort performs sorting an
+        array.
+        This basically implements an insertion sort but places a counter
+        on the inner loop to track the changes.
+        https://en.wikipedia.org/wiki/Insertion_sort
+        '''
+        counter = 0
+        for i in range(1, len(arr)):
+            curval = arr[i]
+            curpos = i
+            while curpos > 0 and arr[curpos - 1] > curval:
+                arr[curpos] = arr[curpos - 1]
+                print(f'Moving value: {curval} from {curpos -1} to {curpos}')
+                counter += 1
+                curpos = curpos - 1
+            arr[curpos] = curval
+        print(arr)
+        return counter
+
+    def insertion_sort_analyse_two(self, arr):
+        count = 0
+        lvar = len(arr)
+        for i in range(lvar):
+            for j in range(i + 1, lvar):
+                if arr[i] > arr[j]:
+                    count += 1
+        return(count)
--- hackerrank/algorithm_sorting_test.py
+++ hackerrank/algorithm_sorting_test.py
@@ -2,9 +2,44 @@
 import algorithm_sorting
 
 
+class TestIntroTutorial(object):
+    def test_intro_tutoral(self):
+        sort1 = algorithm_sorting.Sorting()
+        V = 4
+        arr = [1, 4, 5, 7, 9, 12]
+        result = sort1.intro_tutorial(V, arr)
+        assert(result == 1)
+
+
 class TestMedian(object):
     def test_median_one(self):
         sort1 = algorithm_sorting.Sorting()
         arr = [0, 1, 2, 4, 6, 5, 3]
         result = sort1.median(arr)
         assert(result == 3)
+
+
+class TestInsertionSortAnalyse(object):
+    def test_insertion_sort_analyse_one(self):
+        sort1 = algorithm_sorting.Sorting()
+        arr = [1, 1, 1, 2, 2]
+        result = sort1.insertion_sort_analyse(arr)
+        assert(result == 0)
+
+    def test_insertion_sort_analyse_two(self):
+        sort1 = algorithm_sorting.Sorting()
+        arr = [2, 1, 3, 1, 2]
+        result = sort1.insertion_sort_analyse(arr)
+        assert(result == 4)
+
+    def test_insertion_sort_analyse_three(self):
+        sort1 = algorithm_sorting.Sorting()
+        arr = [1, 1, 1, 2, 2]
+        result = sort1.insertion_sort_analyse_two(arr)
+        assert(result == 0)
+
+    def test_insertion_sort_analyse_four(self):
+        sort1 = algorithm_sorting.Sorting()
+        arr = [2, 1, 3, 1, 2]
+        result = sort1.insertion_sort_analyse_two(arr)
+        assert(result == 4)
