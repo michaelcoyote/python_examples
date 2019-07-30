0ed773bd094e6391286ec29a978dba008c470b7c
michael guldan <michaelguldan@gmail.com>
2019-07-30 11:05:35 -0700

    Improve class example interface
    Better init with bounds checking on various class attributes.
    
    Implemented setter/getter with bounds checking for integer variable
    Migrated existing setter/getter/deleter to use data variable.
    Existing modules are modified or depreciated towards a cleaner interface
    Better docstrings

--- class_examples.py
+++ class_examples.py
@@ -1,53 +1,94 @@
-#!/usr/bin/env python
-"""Comparison of plain, static and class methods.
-
-some tests around staticmethod and classmethod decorators along with methods
-for programmatically loading external data using a dict defined in a static
-method as a map between the external and internal data.
-
-Also shown is the property decorator, with a setter and deleter example.
-
-more info about static and class methods at stackexchange:
-    http://bit.ly/2sAjHEx
-info about property decorators:
-    http://bit.ly/2txQDL5
-"""
+#!/usr/bin/env python3
 
 
-class Test(object):
+class ClassExample(object):
+    """Comparison of plain, static and class methods.
+
+    Some tests around staticmethod and classmethod decorators along
+    with methods for programmatically loading external data using a
+    dict defined in a static method as a map between the external
+    and internal data.
+
+    Also shown is the property decorator, with a setter and deleter
+    examples with some discussion of how these can be used to create a
+    complete interface for accessing variable values within the object
+    without directly accessing the object itself.
+
+    more info about static and class methods at stackexchange:
+        http://bit.ly/2sAjHEx
+    info about property decorators:
+        http://bit.ly/2txQDL5
+    """
+
     def __init__(self, i=1, data=None, stuff=None):
         """Initializer methods configure the class copy.
 
         Assignment of variables and data validation can
         happen here.
+
+        Note that the validation of data can happen within
+        other class modules.  This allows the logic to be
+        reused (e.g. you can use setters not only to access
+        internal vars but do bounds checking and other things
+        and this logic can be reused when an internal variable
+        needs to be updated.
         """
-        self.i = i
-        print('init var {}'.format(self.i))
-        self.data = data
+        self.int_value = i
+        print('init int variable: {}'.format(self.int_value))
+        # Set a default value for self.data if none set at init time
+        if data:
+            self.data = data
+        else:
+            self.data = f"The int value is {self.int_value}"
         self.stuff = stuff or {}
         self.load_stuff(self.stuff)
 
     def __repr__(self):
-        return ('Test(i={}, data={}, stuff={})'.format(self.i,
-                                                       self.data,
-                                                       self.stuff))
+        """Return a printable representation of an object."""
+        return (f"ClassExample(i={self._integer_value}, "
+                f"data={self.data}, stuff={self.stuff})")
 
-    def m_basic(self):
-        print('basic method {}'.format(self.i))
-        return self.i
+    @property
+    def int_value(self):
+        """Getter for self._integer_value."""
+        return self._integer_value
+
+    @int_value.setter
+    def int_value(self, i):
+        """Setter for self._integer_value w/ type check.
+
+        We use raise to throw a TypeError, but other things
+        can be done here like checking bounds, etc."""
+        if isinstance(i, int):
+            self._integer_value = i
+        else:
+            raise TypeError("int_value must be integer")
+
+    def int_multiplier(self):
+        """Return a list of products for the int_value up to 10."""
+        return [i * self.int_value for i in range(1, 11)]
+
+    def m_basic(self, i=None):
+        """Basic method implementation.
+
+        We can set and get with this method but must use the
+        standard interface to do so."""
+        if i is not None:
+            self.int_value = i
+        print('basic method {}'.format(self._integer_value))
+        return self._integer_value
 
     @classmethod
     def m_class(cls):
         """The classmethod accesses the class object directly.
 
         Note that the cls keyword is a shortcut for the
-        actual class name (in this case Test).
+        actual class name (in this case ClassExample).
 
         Use classmethod if you need to access the class methods
         or instances.
         """
-        # i1 = self.i
-        i1 = cls().m_basic()
+        i1 = cls().int_value
         print('class method {}'.format(i1))
         return i1
 
@@ -62,63 +103,85 @@ class Test(object):
         this as it may make sense to split out a staticmethod into
         it's own module instead of having it in the class.
         """
-        # nonclassvar = Test.m_basic()
+        # nonclassvar = ClassExample.m_basic()
         print('static method {}'.format(nonclassvar))
         return nonclassvar
 
     @property
-    def i_method(self):
-        return self.i
+    def data(self):
+        return self._data
 
-    @i_method.setter
-    def i_method(self, new_i=None):
-        if new_i is not None:
-            self.i = new_i
-        return self.i
+    @data.setter
+    def data(self, new_data=None):
+        if new_data is not None:
+            self._data = new_data
+        return self._data
 
-    @i_method.deleter
-    def i_method(self):
-        print('i_method deleter called.')
-        del self.i
+    @data.deleter
+    def data(self):
+        print('data deleter called.')
+        del self._data
 
     @staticmethod
-    def _attributes():
-        """Map between external and internal data structures."""
-        return {
-            'external_1': 'internal1',
-            'external_2': 'internal2',
-            'external_3': 'internal3',
+    def _attributes(ln=3):
+        """Create map for external and internal data structures.
+
+        Again this is a static method with no access to class.
+        This method only creates a map for external to internal
+        data structures so it doesn't need access, and it's specific
+        to creating a map for this class so it's probably fine
+        being in the class as a static method.
+
+        Given a lenght of 3, this method will return a map
+        dictionary something like this:
+
+        map = {
+            'external_1': 'internal_1',
+            'external_2': 'internal_2',
+            'external_3': 'internal_3',
         }
+        """
+        map = {}
+        for i in range(1, (ln + 1)):
+            map[f"external_{i}"] = f"internal_{i}"
+        return map
 
     def load_stuff(self, stuff={}):
         """Load external dict to internal dict using map."""
         loaded = {}
-        for key, value in self._attributes().items():
+        for key, value in self._attributes(len(stuff)).items():
             loaded[value] = stuff.get(key, None)
-            self.int_stuff = loaded
+            self._int_stuff = loaded
         return loaded
 
     def d_stuff(self):
         """Display the internally mapped stuff."""
         for key, value in self._attributes().items():
-            print('{}: {}'.format(value, self.int_stuff.get(value, 'nothing')))
-        return self.int_stuff
+            print('{}: {}'.format(value,
+                                  self._int_stuff.get(value, 'nothing')))
+        return self._int_stuff
 
 
 def main():
-    """Main line of program, duh."""
+    """Main line of program, eh!
+
+    This is mostly just a quick way to illustrate
+    the class examples. Everything has print statements and
+    is noisy as heck.  Note that there is a Test suite
+    where there's a lot of example code calling the class
+    and showing how the examples work."""
 
     # call a classmethod and it initalizes, runs
     # the m_basic def and then itself.
     # can access internal calls.
     print('### classmethod ###')
-    Test.m_class()
+    ClassExample.m_class()
     # call a staticmethod and it runs w/o initialization
     # unless we specifically initialize the class
     print('### staticmethod ###')
-    Test.m_static(i2=2)
+    ClassExample.m_static(nonclassvar=2)
     print('### normal class access ###')
-    test_3 = Test(3)
+    test_3 = ClassExample(3)
     test_3.m_basic()
     print('### load data into data structure  ###')
     # test loading stuff
@@ -126,22 +189,22 @@ def main():
             'external_1': 'a thing 1',
             'external_2': 'another thing 2'
             }
-    test_4 = Test(4)
+    test_4 = ClassExample(4)
     test_4.load_stuff(stuff=stuff)
     test_4.d_stuff()
     # load stuff from __init__
     print('### Same but using __init__() ###')
-    test_5 = Test(5, stuff=stuff)
+    test_5 = ClassExample(5, stuff=stuff)
     test_5.d_stuff()
     # test a property
     print('### property decorators ###')
-    test_6 = Test(6)
-    print('the contents of i: {}'.format(test_6.i_method))
-    test_6.i_method = 66
-    print('updated i: {}'.format(test_6.i_method))
-    del test_6.i_method
+    test_6 = ClassExample(i=6, data='datum')
+    print('the contents of data: {}'.format(test_6.data))
+    test_6.data = 'datas'
+    print('updated data: {}'.format(test_6.data))
+    del test_6.data
     # Representation
-    test_7 = Test(7)
+    test_7 = ClassExample(7)
     print('the representation: {}'.format(repr(test_7)))
 
 
--- test_class_examples.py
+++ test_class_examples.py
@@ -1,37 +1,44 @@
-# import pytest
+import pytest
 import class_examples
 
 
 class TestClassExample(object):
     def test_ex_import(self):
-        ex1 = class_examples.Test()
+        ex1 = class_examples.ClassExample()
         assert(ex1.m_basic() == 1)
 
     def test_ex_getter(object):
-        ex1 = class_examples.Test()
-        assert(ex1.i_method == 1)
+        ex1 = class_examples.ClassExample()
+        assert(ex1.int_value == 1)
 
     def test_ex_setter(object):
-        ex1 = class_examples.Test()
-        assert(ex1.i_method == 1)
-        ex1.i_method = 66
-        assert(ex1.i_method == 66)
+        ex1 = class_examples.ClassExample()
+        assert(ex1.int_value == 1)
+        ex1.int_value = 66
+        assert(ex1.int_value == 66)
+
+    def test_ex_setter_nonint_type_error(self):
+        with pytest.raises(TypeError):
+            class_examples.ClassExample(i="notanint")
+
+    def test_ex_multiplier_function(self):
+        ex1 = class_examples.ClassExample(2)
+        assert(ex1.int_multiplier() == [2, 4, 6, 8, 10, 12, 14, 16, 18, 20])
 
     def test_ex_repr(object):
-        ex1 = class_examples.Test(i=23, data='datum')
+        ex1 = class_examples.ClassExample(i=23, data='datum')
         print(repr(ex1))
-        assert(repr(ex1) == 'Test(i=23, data=datum, stuff={})')
+        assert(repr(ex1) == 'ClassExample(i=23, data=datum, stuff={})')
 
     def test_ex_classmethod(object):
-        assert(class_examples.Test.m_class() == 1)
+        assert(class_examples.ClassExample.m_class() == 1)
 
     def test_internal_external_map(object):
         dict_in = {
                 'external_1': 'thing 1',
                 'external_2': 'thing 2'
                 }
-        ex1 = class_examples.Test()
+        ex1 = class_examples.ClassExample()
         ex1.load_stuff(stuff=dict_in)
-        assert(ex1.d_stuff() == {'internal1': 'thing 1',
-                                 'internal2': 'thing 2',
-                                 'internal3': None})
+        assert(ex1.d_stuff() == {'internal_1': 'thing 1',
+                                 'internal_2': 'thing 2'})
