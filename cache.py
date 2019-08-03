05c88aad2ba3ae0af5e57fa0e943f9a7039f3236
michael guldan <michaelguldan@gmail.com>
2019-08-03 15:47:56 -0700

    Test coverage for cache.py and cleanup of the file

--- cache.py
+++ cache.py
@@ -6,44 +6,62 @@ import random
 
 
 class Cache:
-    """Define a simple cache."""
+    """Implement a simple cache.
 
-    def __init__(self):
-        self.cache = {}
-        self.max_cache_size = 10
+    Cache(max_cache_size=10)
+
+    cache size should be integer
+    """
+
+    def __init__(self, max_cache_size=10):
+        self._cache = {}
+        self.max_cache_size = max_cache_size
 
     def __contains__(self, key):
         """Return a T/F given a key for a cache."""
-        return key in self.cache
+        return key in self._cache
+
+    @property
+    def max_cache_size(self):
+        return self._max_cache_size
+
+    @max_cache_size.setter
+    def max_cache_size(self, max):
+        if max > 0:
+            self._max_cache_size = max
+        else:
+            raise ValueError("max_cache_size needs to be greater than zero")
+
+        return self._max_cache_size
 
     def update(self, key, value):
         """Update and clean cache."""
-        if (key not in self.cache and
-                len(self.cache) >= self.max_cache_size):
+        if (key not in self._cache and
+                len(self._cache) >= self.max_cache_size):
             self.expire_oldest()
-        self.cache[key] = {'date_accessed': datetime.datetime.now(),
-                           'value': value}
+        self._cache[key] = {'date_accessed': datetime.datetime.now(),
+                            'value': value}
 
     def expire_oldest(self):
         """Expire oldest entry."""
         oldest_entry = None
-        for key in list(self.cache):
+        for key in list(self._cache):
             if oldest_entry is None:
                 oldest_entry = key
-            elif (self.cache[key]['date_accessed'] <
-                  self.cache[oldest_entry]['date_accessed']):
+            elif (self._cache[key]['date_accessed'] >
+                  self._cache[oldest_entry]['date_accessed']):
                 print('Removing cache entry: {}'.format(oldest_entry))
-                self.cache.pop(oldest_entry)
+                self._cache.pop(oldest_entry)
                 oldest_entry = None
 
     @property
     def size(self):
         """Cache size."""
-        return len(self.cache)
+        return len(self._cache)
 
 
 def main():
-    keys = ['foo', 'bar', 'baz', 'moo', 'vxx', 'quxx', 'gack', 'florb',
+    keys = ['foo', 'bar', 'baz', 'moo', 'vxx', 'quux', 'gack', 'florb',
             'rarg', 'boop', 'wrm', 'flum', 'mox', 'arrd']
     c = 'abcdefghijklmnop'
     cache = Cache()
@@ -51,9 +69,11 @@ def main():
         if key in cache:
             continue
         else:
-            value = ' '.join([random.choice(c) for i in range(20)])
+            value = ' '.join([random.choice(c) for i in range(30)])
             cache.update(key, value)
-        print('{} iterations, {} cached entries'.format(i+1, cache.size))
+        print('{} iterations, {} keys, {} cached entries'.format(i+1,
+                                                                 len(keys),
+                                                                 cache.size))
     print
 
 
---
+++ test_cache.py
@@ -0,0 +1,38 @@
+import pytest
+import cache
+
+
+# Start the tests here
+class TestCache(object):
+    @pytest.fixture
+    def cache_setup(self):
+        c1 = cache.Cache(max_cache_size=2)
+        for k, v in {'foo': 1, 'bar': 2}.items():
+            c1.update(k, v)
+        return c1
+
+    def test_in_cache(self, cache_setup):
+        """Test template"""
+        assert('foo' in cache_setup)
+
+    def test_not_in_cache(self, cache_setup):
+        assert('baz' not in cache_setup)
+
+    def test_cache_size(self, cache_setup):
+        assert(cache_setup.size == 2)
+
+    def test_force_cache_expiration(self, cache_setup):
+        for k, v in {'baz': 3, 'quux': 4, 'qux': 5}.items():
+            cache_setup.update(k, v)
+        print(cache_setup._cache)
+        assert('foo' not in cache_setup)
+
+    def test_set_cache(self, cache_setup):
+        cache_setup.max_cache_size = 5
+        assert(cache_setup.max_cache_size == 5)
+
+    @pytest.mark.parametrize("size, error", [(-1, ValueError),
+                                             ("10", TypeError)])
+    def test_set_cache_invalid(self, cache_setup, size, error):
+        with pytest.raises(error):
+            cache_setup.max_cache_size = size
